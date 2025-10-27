12785472e592097da75c7eb08c4b5a808500d3a6
jlhyunii <jhpark4692@naver.com>
2025-10-27 22:47:05 +0900

    feat: 전체 자동차 전진 및 차수별 실행 결과 출력 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,8 +1,27 @@
 package racingcar;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        GameInput.readCarnames();
-        GameInput.readTryCount();
+        List<String> names = GameInput.readCarnames();
+        int tryCount = GameInput.readTryCount();
+
+        Cars cars = new Cars(names);
+
+        System.out.println("\n실행 결과");
+
+        for (int i = 0; i < tryCount; i++) {
+            cars.moveAll();
+            printRoundResult(cars);
+        }
+    }
+
+    private static void printRoundResult(Cars cars) {
+        for (Car car : cars.getCars()) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+
+        System.out.println();
     }
 }
--- src/main/java/racingcar/Cars.java
+++ src/main/java/racingcar/Cars.java
@@ -14,4 +14,10 @@ public class Cars {
     public List<Car> getCars() {
         return cars;
     }
+
+    public void moveAll() {
+        for (Car car : cars) {
+            car.move();
+        }
+    }
 }
