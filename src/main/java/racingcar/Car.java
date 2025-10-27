fd92a72080c157147cca6eb6557af0a05240d6da
jlhyunii <jhpark4692@naver.com>
2025-10-27 22:38:50 +0900

    feat: 자동차 전진 로직 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
     private final String name;
     private int position = 0;
@@ -14,5 +16,12 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    public void move() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        if (randomNumber >= 4) {
+            position++;
+        }
     }
 }
